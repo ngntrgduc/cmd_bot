07e840a8dccc4ee74dc4621a713316f0107f5b15
nddkhoa <khoa.nguyen@seedcom.vn>
2021-11-07 00:06:43 +0700

    add shuffle command

--- cogs/random/__init__.py
+++ cogs/random/__init__.py
@@ -14,13 +14,26 @@ class Sample(commands.Cog):
 
     @commands.command(aliases=['choose'])
     async def pick(self, ctx, *, message: str):
-        arr = re.split(',|\|', message)
-        arr = map(lambda s: s.strip(), arr)
-        arr = filter(lambda s: len(s) > 0, arr)
-        arr = list(arr)
-
+        arr = split_message(message)
         response = f':point_right: {random.choice(arr)}'
         await ctx.send(response)
+
+    @commands.command()
+    async def shuffle(self, ctx, *, message: str):
+        arr = split_message(message)
+        random.shuffle(arr)
+        
+        arr = [f'**{index+1}.** {item}' for index, item in enumerate(arr)]
+
+        response = '\n'.join(arr)
+        await ctx.send(response)
+
+def split_message(message):
+    arr = re.split(',|\||\\n', message)
+    arr = map(lambda s: s.strip(), arr)
+    arr = filter(lambda s: len(s) > 0, arr)
+    arr = list(arr)
+    return arr
 
 def setup(client):
     client.add_cog(Sample(client))
