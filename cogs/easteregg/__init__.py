f7a14afb46514af4b9749389a8aca674ab9bd97a
Pn7Hao <91818151+Pn7Hao@users.noreply.github.com>
2021-11-07 01:47:16 +0700

    can get text + img
    :v i dont know ?

--- cogs/easteregg/__init__.py
+++ cogs/easteregg/__init__.py
@@ -6,27 +6,39 @@ class Suprise(commands.Cog):
 
     def __init__(self, client: discord.Client):
         self.client = client
-        self.store =[]
+        self.mess =[]
 
     @commands.Cog.listener()
     async def on_ready(self):
         #print("suprise is runnig")
         pass
 
-    @commands.command(help="Store basic to prepare for a send message")
-    async def get(self, ctx):
-        attachment_url = ctx.message.attachments[0].url
-        #file_request = requests.get(attachment_url)
-        #print(attachment_url)
-        self.store.append(attachment_url)
+    @commands.command(help="Store basic to prepare for a send message include txt/pic or bold ")
+    async def get(self, ctx ,*,message = ""):
+        #setting embed
+        self.embed = discord.Embed(
+            title="Đây là Message từ ai đó?",
+            description=''.join(message),
+            color=discord.Color.blue(),
+        )
+        #setting/check if have a picture where send
+        try:
+            attachment_url = ctx.message.attachments[0].url
+            self.embed.set_image(url=attachment_url)
+        except IndexError:
+            pass
+        await ctx.send("Getted!")
+        #prepare for send
+        self.mess.append(self.embed)
 
-    @commands.command(pass_context = True,help="Send to channel a message which is getted")
-    async def send_to(self,ctx,Id_channel:int,tag:discord.Member =None):
+    @commands.command(pass_context = True,help="Send to channel(id) a message which is getted include tag/tags or not")
+    async def send_to(self,ctx,Id_channel:int,*tags:discord.Member):
+        #send a embeg(message in getted) to Id_channel have(or not) tags person
         channel = self.client.get_channel(int(Id_channel))
         if channel:
-            await channel.send(f'{tag.mention}')
-            if self.store != []:
-                await channel.send(self.store[-1])
+            if len(tags) != 0:
+                await channel.send(" ".join([tag.mention for tag in tags]))
+        await channel.send(embed=self.embed)
 
 def setup(client):
     client.add_cog(Suprise(client))
