6b61eb0fc901504668e7cff0cb1d56842750e6a0
Pn7Hao <91818151+Pn7Hao@users.noreply.github.com>
2021-11-09 16:37:42 +0700

    better cfs feature
    dăm ba cái đồ quỷ này :< vẫn còn bug vs backdoor :> do e cài vào

--- cogs/cfs/__init__.py
+++ cogs/cfs/__init__.py
@@ -3,11 +3,11 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import store
+from datetime import datetime
 
 load_dotenv()
 Var_Channel = os.getenv("VAR_CHANNEL_ID")
 Cfs_Channel = os.getenv("CFS_CHANNEL_ID")
-
 
 class Cfs(commands.Cog):
 
@@ -25,7 +25,8 @@ class Cfs(commands.Cog):
         self.embed = discord.Embed(
             title="Đây là Confession từ ai đó với ngàn yêu thương ?",
             description=''.join(message),
-            color=discord.Color.blue(),
+            color= 0x1aeb5f,
+            timestamp = datetime.utcnow()
         )
         # setting/check if have a picture where send
         try:
@@ -43,31 +44,42 @@ class Cfs(commands.Cog):
             """if len(tags) != 0:
                 await channel.send(" ".join([tag.mention for tag in tags]))"""
             self.msg = await self.var_channel.send(embed=self.embed)
-            await self.msg.add_reaction("✅") # V is accpect
+            self.accept =await self.msg.add_reaction("✅") # V is accpect
             await self.msg.add_reaction("❌") # X is not
             await self.msg.add_reaction("❔") # ? is waiting
 
+        # back_up not fisnish yet
+        back_up = store.get("author")
+        if back_up == None:
+            back_up = []
+        back_up.append(str(sub(str(ctx.author.id))))
+        store.set("author", back_up)
+
+
     # waiting util reaction and send back to cfs channel
     @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user):
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        channel = self.client.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        user = self.client.get_user(payload.user_id)
+        if not user:
+            user = await self.client.fetch_user(payload.user_id)
+        # instead of reaction we should use payload.emoji
+        # for example:
         cfs_channel = self.client.get_channel(int(Cfs_Channel))
-        if (str(reaction.emoji) == '✅') and (user != self.client.user):
-            await self.var_channel.send(f"```Accepted by {user}```")
-            await cfs_channel.send(embed=self.embed)
-        elif (str(reaction.emoji) == '❌') and (user != self.client.user):
-            await self.var_channel.send(f"```No Accepted by {user}```")
-        elif (str(reaction.emoji) == '❔') and (user != self.client.user):
+        now  = datetime.now()
+        current_time  = now.strftime("%H:%M:%S")
+        if (str(payload.emoji) == '✅') and (user != self.client.user) and (payload.channel_id != int(Cfs_Channel)) :
+            await self.var_channel.send(f"```Cfs at  {current_time}  was Accepted by {user}```")
+            await self.msg.delete()
+            self.msg = await cfs_channel.send(embed=self.embed)
+            # await self.msg.clear_reaction("✅") sau là clear reaction
+        elif (str(payload.emoji) == '❌') and (user != self.client.user) and (payload.channel_id != int(Cfs_Channel)):
+            await self.var_channel.send(f"```Cfs at  {current_time} No Accepted by {user}```")
+        elif (str(payload.emoji) == '❔') and (user != self.client.user) and (payload.channel_id != int(Cfs_Channel)):
             await self.var_channel.send(f"```{user} cant process this Trường hợp```")
         else:
             pass
-
-        # back_up not fisnish yet
-        if (store.get("author")) == None:
-            back_up = []
-        else :
-            back_up = store.get("author")
-        back_up.append(str(sub(str(user.id))))
-        store.set("author", back_up)
 
 
 def setup(client):
